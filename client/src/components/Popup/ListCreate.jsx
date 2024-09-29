93b62bf0b2672d87d6bcb0d8d86a4d95c57326b0
Johann Mosin <89855069+AlataO@users.noreply.github.com>
2024-09-29 19:59:36 +0300

    added missing files

---
+++ client/src/components/Popup/ListCreate.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function ListCreate({ show, onClose, onListCreated }) {
+  const [newList, setNewList] = useState({
+    name: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewList({
+      ...newList,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://stride.ddns.net:8080/lists', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newList),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        onListCreated();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>Create New List</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            List Name:
+            <input type="text" name="name" value={newList.name} onChange={handleInputChange} required />
+          </label>
+          <button className="submit-button" type="submit">Create List</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ListCreate;
