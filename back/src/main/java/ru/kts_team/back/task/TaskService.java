a47c63916a12c5ac4ecee19a923c7fac8ddaa801
yberezin <yberezin@cinimex.ru>
2024-09-29 17:43:57 +0300

    feat: add backend logic moments

--- back/src/main/java/ru/kts_team/back/task/TaskService.java
+++ back/src/main/java/ru/kts_team/back/task/TaskService.java
@@ -7,6 +7,14 @@ public interface TaskService {
 
     List<Task> getTasks();
 
+    List<Task> getTasksByListId(Long listId);
+
+    List<Task> getTasksByParentId(Long parentId);
+
+    List<Task> getTasksByIsDone(Boolean isDone);
+
+    List<Task> getTasksByParentIdIsNull();
+
     Task getTaskById(Long id);
 
     Task updateTask(Task task);
